30dac39688acad049fd65f73606d6150a8d0813c
paidaxin35276 <1242233906@qq.com>
2018-03-06 11:18:37 +0800

    3月6日中午 黄鑫提交

---
+++ src/main/java/com/community/entity/MeunEntity.java
@@ -0,0 +1,60 @@
+package com.community.entity;
+
+public class MeunEntity {
+    private Integer id;
+    private String name;
+    private Integer pid;
+    private Integer lv;
+
+    @Override
+    public String toString() {
+        return "MeunEntity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", pid=" + pid +
+                ", lv=" + lv +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public Integer getLv() {
+        return lv;
+    }
+
+    public void setLv(Integer lv) {
+        this.lv = lv;
+    }
+
+    public MeunEntity() {
+    }
+
+    public MeunEntity(Integer id, String name, Integer pid, Integer lv) {
+        this.id = id;
+        this.name = name;
+        this.pid = pid;
+        this.lv = lv;
+    }
+}
